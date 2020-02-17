e477c995988b3d842e45fa5ff85a40358779b81c
Shawn Thompson <plansmash@me.com>
2020-02-16 20:53:24 -0500

    Updates

---
+++ component---src-pages-about-js-b15ef7e91b637ff7b814.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3XHS":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),r=n.n(a),c=n("LbRr");t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c.a,{headerText:"About Gatsby"}),r.a.createElement("p",null,"Such wow. Very React."))}},LbRr:function(e,t,n){"use strict";var a=n("q1tI"),r=n.n(a);t.a=function(e){return r.a.createElement("h1",null,e.headerText)}}}]);
+//# sourceMappingURL=component---src-pages-about-js-b15ef7e91b637ff7b814.js.map
---
+++ component---src-pages-contact-js-7e4bec14fd2d18629d5e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"Cuy+":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),r=n.n(a),c=n("Wbzz"),u=n("LbRr");t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c.Link,{to:"/"},"Home"),r.a.createElement(u.a,{headerText:"Contact"}),r.a.createElement("p",null,"Send us a message!"))}},LbRr:function(e,t,n){"use strict";var a=n("q1tI"),r=n.n(a);t.a=function(e){return r.a.createElement("h1",null,e.headerText)}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-7e4bec14fd2d18629d5e.js.map
